8362f85fdabd419b2d463e47dd0eb1aeef886f47
José Daniel Solano Gracia <usuario3revolt@gmail.com>
2025-09-02 09:08:26 -0500

    m

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App.jsx";
 import Tour from "./Tour/Tour.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/tour" element={<Tour />} />
+      <Route path="*" element={<Navigate to="/public" replace />} />
     </Routes>
   </BrowserRouter>
 );
-
